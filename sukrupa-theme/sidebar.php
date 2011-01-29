b0a2076c32fc362812f4c1ae359a71f50fc2066c
Ted Nielsen <theodore.nielsen@gmail.com>
2011-01-29 22:34:18 +0530

    [nielsen #216] Sample div in sidebar

--- sukrupa-theme/sidebar.php
+++ sukrupa-theme/sidebar.php
@@ -1,54 +1,26 @@
-<div id="sidebar" class="grid_6" style="float:right;margin-left:0px; height: 100%;">
+<div id="sidebar">
 
-<?php
-$query = new WP_Query('showposts=1&meta_key=donormeter');
-?> <div id ="donorsidebar" class ="sidebaritem grid_6"> <?php
-if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-   ?><div class="sidebarheader"><?php
-   the_title();?></div><?php
-   the_excerpt();
-endwhile;
-endif;
-?>
-</div>
+<!--
+	<div id ="donorsidebar" class="sidebarEntry"> 
+		<?php $query = new WP_Query('showposts=1&meta_key=donormeter');
+			  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+		<div class="sidebarHeader"><?php the_title(); ?></div>
+		<div class="sidebarGuts">
+		<?php the_excerpt();
+			endwhile;
+			endif; ?>
+		</div>
+	</div>
+-->
 
-    <div>&nbsp;</div>
-   
-<div id="howhelpsidebar" class="sidebaritem grid_6">
-    <div class="sidebarheader" style="margin-left:0px">How Can I Help?</div>
-    <a href="#">Donate now to Sukrupa</a><br/>
-    <a href="#">Volunteer with Us</a><br/>
-    <a href="#">View our Shop</a><br/>
-</div>
-
-    <div>&nbsp;</div>
-
-    <?php
-$query = new WP_Query('showposts=3&meta_key=events');
-?> <div id ="upcomingeventssidebar" class ="sidebaritem grid_6">
-<div class="sidebarheader" style="margin-left:0px">Upcoming Events</div>
-<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-     the_excerpt();
-endwhile;
-endif;
-?>
-</div>
-
-    <div>&nbsp;</div>
-
-
-<?php
-$query = new WP_Query('showposts=1&meta_key=studentdisplay');
-?> <div id ="studentsidebar" class ="sidebaritem grid_6"> <?php
-if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-    ?><div class="sidebarheader"><?php
-    the_title();?></div><?php
-    the_excerpt();
-endwhile;
-endif;
-?>
-</div>
-
-
+	   
+	<div id="howhelpsidebar" class="sidebarEntry">
+	    <div class="sidebarHeader">How Can I Help?</div>
+	    <div class="sidebarGuts">
+	    	<a href="#">Donate Now to Sukrupa</a><br/>
+	    	<a href="#">Volunteer with Us</a><br/>
+	    	<a href="#">View our Shop</a><br/>
+	    </div>
+	</div>
 
 </div>
