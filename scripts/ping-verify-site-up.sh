c6c304f9de047e50d7f935d6dfab505c985d81d9
Jonathan Andrew Wolter <jawolter@gmail.com>
2011-02-01 23:41:05 +0530

    [jaw, Renaud] #182 autodeploying wordpress, with correct exit in script

--- scripts/ping-verify-site-up.sh
+++ scripts/ping-verify-site-up.sh
@@ -5,6 +5,6 @@ site_to_fetch=$1
 wget -q --spider http://$site_to_fetch/ || exit 1
 
 expected_text_on_site='Proudly powered by WordPress.'
-test  `wget -qO- http://$site_to_fetch | grep  "$expected_text_on_site" | wc -l ` == 1 || ( echo 'Site not displaying expected content.';  echo 'die' )
+test  `wget -qO- http://$site_to_fetch | grep  "$expected_text_on_site" | wc -l ` == 1 || ( echo 'Site not displaying expected content.';  exit 1 )
 
 echo 'site up OK and seems to be hosting expected content'
