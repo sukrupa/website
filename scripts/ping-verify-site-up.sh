225e8ccda3e0688554d7d2c37b6d37081ccaacbc
Jonathan Andrew Wolter <jawolter@gmail.com>
2011-02-01 23:02:12 +0530

    [jaw, Renaud] #182 autodeploying wordpress

---
+++ scripts/ping-verify-site-up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+site_to_fetch=$1
+
+wget -q --spider http://$site_to_fetch/ || exit 1
+
+expected_text_on_site='Proudly powered by WordPress.'
+test  `wget -qO- http://$site_to_fetch | grep  "$expected_text_on_site" | wc -l ` == 1 || ( echo 'Site not displaying expected content.';  echo 'die' )
+
+echo 'site up OK and seems to be hosting expected content'
