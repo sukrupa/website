f9f5581bd7f5170c3ab84af1b2b36378b50f8d69
Ted Nielsen <theodore.nielsen@gmail.com>
2011-02-04 11:06:09 +0530

    [nielsen] Fixed critical error on page.php

--- sukrupa-theme/page.php
+++ sukrupa-theme/page.php
@@ -4,6 +4,7 @@
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<h2><?php the_title(); ?></h2>
 		<?php the_content(); ?>
+	<?php endwhile; endif; ?>
 </div>
 
 <?php get_sidebar(); get_footer(); ?>
--- sukrupa-theme/sidebar.php
+++ sukrupa-theme/sidebar.php
@@ -4,18 +4,19 @@
 	<div class="sidebarEntry"> 
 		<?php $query = new WP_Query('showposts=1&meta_key=donormeter&post_type=page');
 			  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-		<div class="sidebarHeader"><?php the_title(); ?></div>
+		<div class="sidebarHeader"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
 		<div class="sidebarGuts">
 		<?php $donormeter = get_post_meta($post->ID,'donormeter',true);
 			  $donormeter = explode('/', $donormeter);
 			  $percentage = $donormeter[0]/$donormeter[1];
 			  $width = 200 * $percentage; ?>
 			<div class="baseMeter">
+				<!-- <a href="<?php the_permalink(); ?>" id="capitalCampaign"><?php the_title(); ?></a> -->
 				<span>Rs. <?php echo $donormeter[1];?></span>
 				<div style="width:<?php echo $width; ?>px;" class="progressMeter"></div>
 			</div>
 
-			<?php the_excerpt(); ?>
+			<div style="float:left;margin-top:10px;"><?php the_excerpt(); ?></div>
 		<?php endwhile; endif; ?>
 		</div>
 	</div>
