c6e839687b52ba4feab7cba1579bf71147ac5fd1
Ted Nielsen <theodore.nielsen@gmail.com>
2011-02-04 10:47:20 +0530

    [nielsen] Template prep

--- sukrupa-theme/home.php
+++ sukrupa-theme/home.php
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
 <div class="grid_18">
 	<div id="photocarousel">
-    <?php $query = new WP_Query('showposts=3&meta_key=carousel');
+    <?php $query = new WP_Query('showposts=3&meta_key=carousel&post_type=any');
 		  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
   	      $featuredPhoto = get_post_meta($post->ID,'carousel',true); ?>
     	<div class="carouselEntry">
@@ -35,12 +35,23 @@ $(document).ready(function() {
 	
 	<div class="grid_9 container col" style="margin-left: 15px;">
 	    <h2>Current News</h2>
-	    <?php $query = new WP_Query('category_name=news&showposts=5');
-	    if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-	        the_title('<li>', '</li>', true);
+		<?php 
+		// Category is a more stable query basis; with 'meta_key' the client will need to unneccessiarily
+		// tag items (especially since the meta_key has a null and unused value)
+
+		$query = new WP_Query('category_name=news&showposts=5');
+            
+	    if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+	    
+	    <div id="newspost">
+	    	<h3><a style="color:inherit;" href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+	    	<?php // Change to thumbnail to support naming conventions.
+                $thumbnail = get_post_meta($post->ID,'thumbnail',true);
+                if($thumbnail): ?><p><img src=" <?php echo $thumbnail; ?>" class="newsthumbnail" alt="Featured Photo" width="100"/><?php endif;
 	        the_excerpt();
-	    endwhile;
-	    endif;
+                ?></p></div><br/><?php
+                endwhile;
+	        endif;
 	?>
 	</div>
 	   
--- sukrupa-theme/sidebar.php
+++ sukrupa-theme/sidebar.php
@@ -2,13 +2,21 @@
 
 
 	<div class="sidebarEntry"> 
-		<?php $query = new WP_Query('showposts=1&meta_key=donormeter');
+		<?php $query = new WP_Query('showposts=1&meta_key=donormeter&post_type=page');
 			  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 		<div class="sidebarHeader"><?php the_title(); ?></div>
 		<div class="sidebarGuts">
-		<?php the_excerpt();
-			endwhile;
-			endif; ?>
+		<?php $donormeter = get_post_meta($post->ID,'donormeter',true);
+			  $donormeter = explode('/', $donormeter);
+			  $percentage = $donormeter[0]/$donormeter[1];
+			  $width = 200 * $percentage; ?>
+			<div class="baseMeter">
+				<span>Rs. <?php echo $donormeter[1];?></span>
+				<div style="width:<?php echo $width; ?>px;" class="progressMeter"></div>
+			</div>
+
+			<?php the_excerpt(); ?>
+		<?php endwhile; endif; ?>
 		</div>
 	</div>
 
@@ -25,11 +33,11 @@
 
 
     <div class="sidebarEntry">
-        <?php $query = new WP_Query('showposts=3&meta_key=events');?>
-	    <div class="sidebarHeader">Upcoming Events</div>
+        <?php $query = new WP_Query('showposts=4&category=events');?>
+	    <div class="sidebarHeader">Events</div>
 	    <div class="sidebarGuts">
 	    	<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-                    the_excerpt();
+                    the_title('<h3>', '</h3>');
                 endwhile;
                 endif;
                 ?>
