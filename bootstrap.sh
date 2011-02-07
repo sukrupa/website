27a2b52458906e724a43de752eed8198ce2b3436
suhas <vishwas@thoughtworks.com>
2011-02-07 19:50:15 +0530

    [suhas,jaw] #182 updated the db dump from twu-staging server, and it includes the correct wp theme as an option now. Began moving stuff away from the capastrano script to bootstrap.sh. Updated wp-config.php to now use the twu-staging database.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,19 @@
-echo "************************************************************************************************************"
-echo "PREREQUISITES: "
-echo "1) You need to install MySQL community edition server"
-echo "2) You need to enable on apache the php module."
-echo "   (On mac, uncomment in /etc/apache2/httpd.conf: LoadModule php5_module libexec/apache2/libphp5.so)"
-echo "3) The go user must be able to run '/bin/ln' and '/etc/init.d/apache2 restart' as sudo without a password."
-echo "************************************************************************************************************"
+#!/bin/bash
 
-die () {
+ROOT_MYSQL_PASSWORD='root'
+
+usage() {
+cat <<EOF
+usage: $0 [options]
+
+OPTIONS:
+  -R	reset, totally wipe out local database and installed files
+  -d	configure ci and staging deployment environments
+  -l    configure local development environments (including to use staging server db)
+EOF
+}
+
+die() {
     echo >&2 "$@"
     echo "Fatal error, exiting"
     exit 1
@@ -21,39 +28,87 @@ die_if_not_on_path() {
     die "!!  Is $1 installed? It is not found on your path. Exiting... !!";
   fi
 }
-die_if_not_on_path 'mysql'
-die_if_not_on_path 'mysqladmin'
 
-ROOT_MYSQL_PASSWORD='root'
+prerequisites() {
+    ls `pwd`/sukrupa-theme
+    die_if_errors "You must run this from the root directory of the project"
 
-echo ""
-echo "Bootstraping wordpress configuration. (With default mysql 'root' user and password '$ROOT_MYSQL_PASSWORD') Creating database."
-echo "If you do not have the mysql root password correct, run this command: mysqladmin -u root password root"
+	echo "************************************************************************************************************"
+	echo "PREREQUISITES: "
+	echo "*) You need to enable apache's mod_php5, mod_rewrite, and mod_substitute."
+	echo ""
+	echo "FOR CI AND STAGING SERVERS:"
+	echo "*) You need to install MySQL community edition server, sudo apt-get install mysql-server php5mysql"
+	echo "*) The go user must be able to run '/bin/ln' and '/etc/init.d/apache2 restart' as sudo without a password."
+	echo "   i.e. in /etc/sudoers:  go ALL = (root) NOPASSWD: /bin/ln, /etc/init.d/apache2 restart"
+	echo ""
+	echo "FOR LOCAL DEVELOPMENT:"
+	echo "*) Create a conf/apache-sukrupa-dev.conf with appropriate paths for your environment."
+	echo "   Enable the 'Substitute' lines in module or it will show the wrong content (!! IMPORTANT !!)"
+	echo "   Link it to apache. "
+	echo "   i.e. on linux:"
+	echo " 		sudo ln -s <ABSOLUTE path to>/conf/apache-sukrupa-dev.conf /etc/apache2/sites-enabled/"
+	echo "   i.e. on mac:"
+	echo "		sudo ln -s <ABSOLUTE path to>/conf/apache-sukrupa-dev.conf /etc/apache2/other/"
+	echo "   and then on both, restart apache"
+	echo "*) Edit /etc/hosts to include: 127.0.0.1 sukrupa.localhost"	
+	echo "************************************************************************************************************"
+}
 
-mysql -uroot -p$ROOT_MYSQL_PASSWORD --verbose --execute="create database if not exists sukrupa_wordpress default character set 'utf8';" 
-die_if_errors
+create_database_if_missing(){
+    die_if_not_on_path 'mysql'
+    die_if_not_on_path 'mysqladmin'
 
-mkdir -p installed-wordpress/
-mkdir -p logs/
+	echo "- Bootstraping wordpress configuration. (With default mysql 'root' user and password '$ROOT_MYSQL_PASSWORD') Creating database."
+	echo "  If you do not have the mysql root password correct, run this command: mysqladmin -u root password root"
 
-# TODO: add step to clean (and back up) any existing installed-wordpress/ directory, and then initialize the template directory
+	mysql -uroot -p$ROOT_MYSQL_PASSWORD --verbose --execute="create database if not exists sukrupa2_wordpress default character set 'utf8';" 
+	die_if_errors
+}
 
-echo "extracting wordpress binary"
-rm -rf installed-wordpress/
-unzip -qo lib/wordpress-3.0.4.zip -d .
-mv wordpress installed-wordpress
+install_wordpress(){
+	echo "- extracting wordpress binary"
+	mkdir -p logs/
+	rm -rf installed-wordpress/
+	unzip -qo lib/wordpress-3.0.4.zip -d .
+	mv wordpress installed-wordpress
+	cp -f lib/wp-config.php installed-wordpress/
+	cp -f lib/.htaccess installed-wordpress/
+	ln -sf `pwd`/sukrupa-theme/ ./installed-wordpress/wp-content/themes/sukrupa
+}
 
-cp -f lib/wp-config.php installed-wordpress/
-cp -f lib/.htaccess installed-wordpress/
+reset_db() {
+    echo "- first creating backup of old database tables before dropping everything"
+    timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
+    mysqldump sukrupa_wordpress -uroot -p$ROOT_MYSQL_PASSWORD > database-backups/sukrupa_wordpress.backupdump.$timestamp.sql   
+    die_if_errors
+    echo "  (If the database migration was successful, you may want to remove the backup db dump.)"
+    echo "- now dropping everything from the database and recreating tables"
+    mysql -uroot -p$ROOT_MYSQL_PASSWORD sukrupa_wordpress < lib/local_sukrupa_wordpress.dump.sql
+    die_if_errors
+}
 
-echo "first creating backup of old database tables before dropping everything"
-timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
-mysqldump sukrupa_wordpress -uroot -p$ROOT_MYSQL_PASSWORD > database-backups/sukrupa_wordpress.backupdump.$timestamp.sql   
-die_if_errors
 
-#echo "now dropping everything from the database and recreating tables"
-#mysql -uroot -p$ROOT_MYSQL_PASSWORD sukrupa_wordpress < lib/local_sukrupa_wordpress.dump.sql
-#die_if_errors
 
-echo ""
-echo "(If the database migration was successful, you may want to remove the backup db dump.)"
+while getopts "Rdl" OPTION
+do
+	case $OPTION in
+		R)
+		    reset_db
+			;;
+		d)
+			prerequisites		
+			create_database_if_missing
+			install_wordpress
+			;;
+	    l)
+	        prerequisites
+	        install_wordpress
+	        ;;	
+	    *)
+	        usage
+	        ;;        
+	esac
+done
+
+
