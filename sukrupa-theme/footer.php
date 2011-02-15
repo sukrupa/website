fb65e0754438374953a836382f6b1bdaa3902f95
Ted Nielsen <theodore.nielsen@gmail.com>
2011-02-15 11:27:28 +0530

    [nielsen] #191, Added Google Analytics

--- sukrupa-theme/footer.php
+++ sukrupa-theme/footer.php
@@ -31,5 +31,20 @@
 <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery.masonry.js"></script>
 <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/custom.js"></script>
 <?php endif; ?>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21412788-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </body>
 </html>
