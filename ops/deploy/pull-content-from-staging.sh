735950d97576f7eb0338d51c9fb4db52672390cc
Ron <rjanusz@thoughtworks.com>
2011-02-17 11:58:59 +0530

    [Minno, Ron] #293 wrote a script to sync staging content to local machine for git committing

---
+++ ops/deploy/pull-content-from-staging.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "this must be run in the directory holding lib, such as this:"
+echo "sh ops/deploy/pull-content-from-staging.sh"
+if [ ! -f lib/sukrupa_wordpress.dump.sql ]; then
+   echo 'you appear to not be in the correct directory, exiting'
+   exit 1
+fi
+
+# "backup latest twu-staging wordpress database"
+echo "backing up..."
+mysqldump -uroot -proot -htwu-staging sukrupa_wordpress > lib/sukrupa_wordpress.dump.sql
+
+echo "syncing the staging server to your local machine"
+rsync --verbose --backup --progress --stats --compress --recursive --times --perms --links twu@twu-staging:/var/opt/sukrupa/sukrupa-website/content/ ./contentTemp/
+
+
+echo ''
+git status
+echo ''
+echo 'now you can commit'
+
