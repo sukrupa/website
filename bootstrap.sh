98ef5316098b83921158c9c58b655150d54e3c20
Fabio Rehm <fgrehm@gmail.com>
2011-03-30 17:21:42 +0530

    [FabioR, UgochiO] #387 Fixing image verification for volunteer form

--- bootstrap.sh
+++ bootstrap.sh
@@ -99,7 +99,7 @@ install_wordpress() {
     cp -f content/favicon.ico installed-wordpress/
     ln -sf `pwd`/sukrupa-theme/ ./installed-wordpress/wp-content/themes/sukrupa
     ln -sf `pwd`/content/ ./installed-wordpress/content
-    ln -sf `pwd`/plugins/sukrupa-volunteer-form ./installed-wordpress/wp-content/plugins/sukrupa-volunteer-form
+    cp -fr `pwd`/plugins/sukrupa-volunteer-form ./installed-wordpress/wp-content/plugins/
 }
 
 reset_db() {
