71404ae9cbc19a587d1502a8447d13f668638f08
Fabio Rehm <fgrehm@gmail.com>
2011-04-12 09:58:47 +0530

    [FabioR, FrankT] #431 Changing plugin to display the events calendar

--- plugins/sukrupa-calendar/sukrupa-calendar.php
+++ plugins/sukrupa-calendar/sukrupa-calendar.php
@@ -9,7 +9,7 @@ Version: 1.0
 Author URI: http://www.thoughtworks.com
 */
 
-define('VOLUNTEER_CALENDAR_HTML', '<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=volunteer%40sukrupa.org&amp;color=%23182C57&amp;src=en.indian%23holiday%40group.v.calendar.google.com&amp;color=%23691426&amp;ctz=Asia%2FCalcutta" style=" border-width:0 " width="675" height="500" frameborder="0" scrolling="no"></iframe>');
+define('VOLUNTEER_CALENDAR_HTML', '<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=events%40sukrupa.org&amp;color=%23182C57&amp;src=en.indian%23holiday%40group.v.calendar.google.com&amp;color=%23691426&amp;ctz=Asia%2FCalcutta" style=" border-width:0 " width="675" height="500" frameborder="0" scrolling="no"></iframe>');
 
 add_filter('the_content', 'check_content');
 
