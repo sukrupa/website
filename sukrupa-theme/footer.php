1caccf7dc91cd6fffcb3a6c368ce98979151628f
Jim Barritt <jim@planet-ix.com>
2011-04-15 12:56:22 +0530

    [JimB] Updating google analytics id to be the techs@sukrupa account

--- sukrupa-theme/footer.php
+++ sukrupa-theme/footer.php
@@ -35,7 +35,7 @@
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-21412788-1']);
+  _gaq.push(['_setAccount', 'UA-22755451-1']);
   _gaq.push(['_trackPageview']);
 
   (function() {
