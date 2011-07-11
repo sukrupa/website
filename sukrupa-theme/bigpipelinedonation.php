da51b68067ce155690ca5d59c1a019515d2c4e05
Kishore kumar <abyu.kishore@gmail.com>
2011-07-11 14:18:22 +0530

    Sree n Vishnu; Pushing in spike of Big pipe line pop up

--- sukrupa-theme/bigpipelinedonation.php
+++ sukrupa-theme/bigpipelinedonation.php
@@ -4,8 +4,14 @@
 Template Name: Big PipeLine Donations Page
 */
 ?>
+
 <div>
-<h3>You are donating to the Power Generator</h3>
-    <p>Please mention the same in "Add a message" option</p>
-<iframe src='https://www.ammado.com/nonprofit/118838/givingwidget?alternativeControllerNameForView=Donate' width="500" height="500" frameborder='0'> </iframe>
+   <?php $query = new WP_Query('showposts=1&meta_key=donormeter&post_type=page');
+			  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+    <p>You are donating to the <?php the_title(); ?></p>
+    
+    <p>Please mention the same in "Add a message" option before proceeding with your payment.</p>
+    <iframe src='https://www.ammado.com/nonprofit/118838/givingwidget?alternativeControllerNameForView=Donate' width="500" height="500" frameborder='0'> </iframe>
+    <?php
+    endwhile; endif; ?>
  </div>
