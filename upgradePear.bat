cd70f03c2f89e5fb889f5ed0efa8182a4513f76a
Pooja Akshantal <pooja.akshantal@yahoo.com>
2011-07-18 09:22:58 +0530

    modified upgradePear.bat

--- upgradePear.bat
+++ upgradePear.bat
@@ -1,15 +1,9 @@
 cd C:/xampp/php
 call go-pear.bat
-call pear-update.bat
 call pear clear-cache
 call pear channel-discover pear.phpunit.de
 call pear channel-discover components.ez.no
 call pear channel-discover pear.symfony-project.com
-call pear upgrade-all
-call pear install --nodeps XML_RPC2
 call pear install phpunit/PHPUnit
-call pear upgrade-all
-call pear clear-cache
-call pear install --alldeps --force phpunit/PHPUnit
 
 
