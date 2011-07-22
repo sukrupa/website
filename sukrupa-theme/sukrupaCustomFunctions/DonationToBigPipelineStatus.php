0f43c7be575907f482d38211606d6f012333fa20
Pooja Akshantal <pooja.akshantal@yahoo.com>
2011-07-22 12:07:54 +0530

    Rajashree/Pooja Added Donated amount to the progress bar

--- sukrupa-theme/sukrupaCustomFunctions/DonationToBigPipelineStatus.php
+++ sukrupa-theme/sukrupaCustomFunctions/DonationToBigPipelineStatus.php
@@ -4,6 +4,7 @@
 class DonationToBigPipelineStatus {
 
     private $_sukrupaRequestHandler;
+    const ROUNDING_TO_5_PERCENT = 5;
 
     function __construct(SukrupaRequestHandler $sukrupaRequestHandlerInput)
     {
@@ -23,6 +24,19 @@ class DonationToBigPipelineStatus {
     public function getAmountDonatedToBigPipeLineItem(){
         $highPriorityBigNeedItemInfo=$this->_sukrupaRequestHandler->requestData("./getHighPriorityBigNeedItem/amountDonated");
         return $highPriorityBigNeedItemInfo->{'bigNeedItemAmountDonated'};
+    }
+
+    public function progressComplete()
+    {
+        $totalCostOfBigNeed = $this->getTotalCostOfBigPipelineItem();
+        $amountDonatedForBigNeed = $this->getAmountDonatedToBigPipeLineItem();
+
+        $percentageOfDonatedAmountAbsolute = $amountDonatedForBigNeed / $totalCostOfBigNeed * 100;
+        $roundedPercentage=((int)($percentageOfDonatedAmountAbsolute/ self::ROUNDING_TO_5_PERCENT))* self::ROUNDING_TO_5_PERCENT;
+
+        if ($roundedPercentage>=90)
+            $roundedPercentage=85;
+        return $roundedPercentage;
     }
 
 }
