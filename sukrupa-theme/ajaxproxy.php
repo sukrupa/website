12b1723c873acb5a70a7a378f872051bf3afff77
Ben Barnard <barnardb@gmail.com>
2011-07-22 14:32:38 +0530

    Ben/Matt: stop-gap for deployment: for school admin server use localhost:8080 if we are accessed as localhost, use school.sukrupa.org otherwise

--- sukrupa-theme/ajaxproxy.php
+++ sukrupa-theme/ajaxproxy.php
@@ -1,5 +1,10 @@
 <?php
-$c = curl_init('http://localhost:8080/addsubscriberinfo?subscriberName=' . $_POST['subscriberName'] . '&subscriberEmail=' . $_POST['subscriberEmail']);
+if ( $_SERVER['HTTP_HOST'] == 'localhost' ) {
+    $school_admin_server = 'http://localhost:8080';
+} else {
+    $school_admin_server = 'http://school.sukrupa.org';
+}
+$c = curl_init($school_admin_server . '/addsubscriberinfo?subscriberName=' . $_POST['subscriberName'] . '&subscriberEmail=' . $_POST['subscriberEmail']);
 curl_exec($c);
 $info = curl_getinfo($c);
 curl_close($c);
