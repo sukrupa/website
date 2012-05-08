d947a6b4617f1b371e216c49f42269803b24915e
Jonathan Andrew Wolter <jwolter@secondvalley.com>
2012-05-07 22:51:01 -0700

    JAW - fixing site for now, becuse it appears school.sukrupa.org is down. I removed all that code.

--- sukrupa-theme/sidebar.php
+++ sukrupa-theme/sidebar.php
@@ -2,124 +2,17 @@
   // Includes plugin file in order to check if plugin was activated
   $sDir = dirname(__FILE__);
   require_once($sDir . '/../installed-wordpress/wp-admin/includes/plugin.php');
-  include("sukrupaCustomFunctions/SponsoredChildrenStatus.php");
-  include("sukrupaCustomFunctions/SukrupaRequestHandler.php");
-  include("sukrupaCustomFunctions/DonationToBigPipelineStatus.php");
-  include("sukrupaCustomFunctions/DonationToSmallPipelineStatus.php");
-  $sponsorshipWidget = new SponsoredChildrenStatus(new SukrupaRequestHandler());
-  $sponsoredStudentsCount = $sponsorshipWidget->getNumberOfStudentsSponsored();
-  $totalStudentsCount = $sponsorshipWidget->getNumberOfStudents();
-  $bigNeedDonationStatus = new DonationToBigPipelineStatus(new SukrupaRequestHandler());
-  $highPriorityBigNeedItem = $bigNeedDonationStatus->getHighPriorityBigPipelineItem();
-  $totalCostForHighPriorityBigNeedItem = $bigNeedDonationStatus->getTotalCostOfBigPipelineItem();
-  $amountDonatedToHighPriorityBigNeedItem = $bigNeedDonationStatus->getAmountDonatedToBigPipeLineItem();
-  $smallNeedDonationStatus= new DonationToSmallPipelineStatus(new SukrupaRequestHandler());
-  $smallNeedList = $smallNeedDonationStatus->getSmallPipelineItems();
-
 ?>
 
 <div id="sidebar">
-<div id="slider">
-
-
-
+  <div id="slider">
     <div class="sidebarEntry">
 		<div class="sidebarHeader"><a href="<?php bloginfo('home'); ?>/sponsor/">Sponsor A Child</a></div>
         <div class="sidebarGuts">
-            <div class="progressBar">
-                <div class="percent<?php echo $sponsorshipWidget->progressComplete(); ?>"><p class="progressMarker"><?php echo $sponsoredStudentsCount;  ?>/<?php echo $totalStudentsCount; ?></p></div>
-            </div>
-
-            <p><?php echo $totalStudentsCount-$sponsoredStudentsCount; ?> children need a sponsor.</br> <a href="<?php bloginfo('home'); ?>/sponsor/">Click here</a> to Sponsor a child</p>
+            <p><a href="<?php bloginfo('home'); ?>/sponsor/">Click here</a> to Sponsor a child</p>
         </div>
     </div>
-    <span class="sidebarEntry">
-        <?php echo $sponsorshipWidget->getErrorMessageIfAny(); ?>
-    </span>
-
-
-	<div class="sidebarEntry"> 
-		<?php $query = new WP_Query('showposts=1&meta_key=donormeter&post_type=page');
-			  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-		<div class="sidebarHeader">
-            <a href="<?php bloginfo('home'); ?>/big-pipe-line-donation"> <?php echo $highPriorityBigNeedItem; ?></a>
-        </div>
-		<div class="sidebarGuts">
-
-                 <!--   <a href="<?php bloginfo('home'); ?>/big-pipe-line-donation/"><?php the_title(); ?></a>   -->
-                       
-
-		<?php $donormeter = get_post_meta($post->ID,'donormeter',true);
-			  $donormeter = explode('/', $donormeter);
-			  $amountDonatedToBigNeedItem = (int) str_ireplace(",", "", $amountDonatedToHighPriorityBigNeedItem );
-			  $totalCostForBigNeedItem = (int) str_ireplace(",", "", $totalCostForHighPriorityBigNeedItem );
-			  
-			  if ( !is_int( $amountDonatedToBigNeedItem ) || !is_int( $totalCostForBigNeedItem ) ) { continue; }
-			  if ( $amountDonatedToBigNeedItem <= 0 || $totalCostForBigNeedItem < 0) {$percentage=0;}
-              else {$percentage = $amountDonatedToBigNeedItem/$totalCostForBigNeedItem;}
-			  
-
-			  $width = 200 * $percentage; 
-			  if ( $width > 200 ) { $width = 200; }
-			  ?>
-			<div class="baseMeter">
-				<!-- <a href="<?php the_permalink(); ?>" id="capitalCampaign"><?php the_title(); ?></a> -->
-				<p>Rs. <?php echo number_format( $totalCostForBigNeedItem );?></p>
-				<div style="width:<?php echo $width; ?>px;" class="progressMeter"></div>
-			</div>
-          <a href="<?php bloginfo('home'); ?>/big-pipe-line-donation">Click here to donate</a>
-          </div>
-
-
-
-
-			<div style="float:left;margin-top:10px;color:black" >
-			<p style="font-size:15px">Join our current fundraising
-			campaign to purchase a new <?php echo
-			$highPriorityBigNeedItem; ?> for SUKRUPA by <a
-			style="color: #0000ff; font-size: 90%"
-			href="<?php bloginfo('home');
-			?>/big-pipe-line-donation">clicking here!</a></p></div>
-		<?php endwhile; endif; ?>
-		</div>
 	</div>
-
-
-
-  <div class="sidebarEntry">
-		<?php $query = new WP_Query('showposts=1&meta_key=donormeter&post_type=page');
-			  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-		<div class="sidebarHeader">Small Needs </div>
-
-		<div class="sidebarGuts">
-              <table id="smallNeedsTable">
-                <thead>
-                    <td><b>Item Name</b></td><td><b>Cost</b></td>
-                </thead>
-                  <tbody>
-                       <?php
-                       $smallPipelineDonationPageLink = get_bloginfo('home');
-                       $smallPipelineDonationPageLink.='/small-pipe-line-donation';
-                       $itemName=str_replace(" ","-",$smallNeedList[$i]);
-                       if(sizeof($smallNeedList) <=10)
-                            $listCount=sizeof($smallNeedList);
-                       else
-                            $listCount=10;
-                       for($i=0;$i<$listCount;$i=$i+2)
-                        echo  "<tr>
-                            <td><a href=$smallPipelineDonationPageLink?itemName=".str_replace(" ","-",$smallNeedList[$i]).">" .$smallNeedList[$i]."</a></td>
-                            <td>".(int)$smallNeedList[$i+1] ."</td>
-                        </tr>";
-                        ?>
-                  </tbody>
-              </table>
-          </div>
-
-			<div style="float:left;margin-top:10px;color:black" >Click on the item to donate</div>
-		<?php endwhile; endif; ?>
-		
-	</div>
-
 
 <div class="sidebarEntry">
      <div class="sidebarHeader">Create an Opportunity</div>
@@ -139,8 +32,6 @@
          <!-- <a href="http://www.sukrupa.org/creations.html" target="_blank">View our Shop</a><br/> -->
      </div>
  </div>
-
-
 
     <?php $query = new WP_Query('showposts=4&category_name=events'); if ( $query->have_posts() ) :?>
     <div class="sidebarEntry">
@@ -172,22 +63,15 @@
      FROM THE R.T. NAGAR POLICE STATION
      <ul>
          <li>- HMT Grounds are diagonally opposite to Police Station.</li>
-
          <li>- Come past HMT Grounds and past Karnataka Flag Post.</li>
-
          <li>- Turn RIGHT at dead end.</li>
-
          <li>- Come to next dead end (Salamat School) and turn LEFT.</li>
-
          <li>- Come past Mamatha High School and Shakthi Bakery (on right).</li>
-
          <li>- Cross drainage bridge and continue straight; road will curve to the RIGHT.</li>
-
          <li>- SUKRUPA Education Center is at the junction of the first left.</li>
-
          <li>- Continue to dead end and turn LEFT. SUKRUPA Administration on RIGHT (three story green and pink building).</li>
          <ul>
- </div>
+     </div>
  </div>
 
 <div class="sidebarEntry">
@@ -200,9 +84,6 @@
                 endif;
     ?>
 </div>
-
 </div>
-
-
 </div>
 </div>
