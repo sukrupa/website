b02b9f3d5f1cecdd1cf5a69b2fa0a53894e5df59
Noppanit Charassinvichai <noppanit.c@gmail.com>
2012-09-04 17:39:16 +0200

    Changed to MailChimp

--- sukrupa-theme/home.php
+++ sukrupa-theme/home.php
@@ -43,7 +43,32 @@ $(document).ready(function() {
 	
 	<div id="current-news" class="grid_9 container col" style="margin-left: 15px;">
 	    <h2>Current News</h2>
-        <a class="icon news-icon" href="<?php bloginfo('home'); ?>/subscribeform/">Subscribe to newsletter</a>
+        <!-- Begin MailChimp Signup Form -->
+		<link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+		<style type="text/css">
+			#mc_embed_signup {
+				background: #fff;
+				clear: left; 
+				font: 14px Helvetica,Arial,sans-serif; 
+			}
+			/* Add your own MailChimp form style overrides in your site stylesheet
+			or in this style block.
+			  We recommend moving this block and the preceding CSS link to the
+			HEAD of your HTML file. */
+		</style>
+		<div id="mc_embed_signup">
+			<form action="http://sukrupa.us5.list-manage.com/subscribe/post?u=f3a131214c81db7544cb91239&amp;id=bb567124fc" method="post" id="mc-embedded-subscribe-form"
+			name="mc-embedded-subscribe-form" class="validate" target="_blank">
+			<label for="mce-EMAIL">Subscribe to our mailing list</label>
+			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL"
+			placeholder="email address" required>
+			<div>
+				<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+			</div>
+			</form>
+		</div>
+
+<!--End mc_embed_signup-->
 		<?php 
 		// Category is a more stable query basis; with 'meta_key' the client will need to unneccessiarily
 		// tag items (especially since the meta_key has a null and unused value)
